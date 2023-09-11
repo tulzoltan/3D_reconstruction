e0edf080f3bc26870c272075c6a511255d30844c
Zoltan Tulipant <31505476+tulzoltan@users.noreply.github.com>
2023-09-11 10:30:24 +0200

    Add files via upload

--- undistort.py
+++ undistort.py
@@ -3,72 +3,88 @@ import numpy as np
 import cv2 as cv
 
 
-def get_calibration_data(calibration_file, camera_names):
-    """Get intrinsic matrix and distortion parameters from calibration file"""
-
-    CameraInfo = {}
+def get_masks(calibration_file, camera_names):
+    """Get file names for misalignment and obstruction masks."""
+    mask_files = {}
 
     with open(calibration_file) as in_file:
         data = json.load(in_file)
         for cname in camera_names:
-            cdata = data[cname]
+            cdata=data[cname]["custom_vars"]
+            mask_files[cname] = {
+                    "mis_mask": cdata["misalign_mask_file"],
+                    "mis_ref":  cdata["misalign_ref_file"],
+                    "obs_mask": cdata["obstruction_mask_file"],
+                    }
 
-            #Construct intrinsic matrix
-            Intrinsic = np.eye(3)
-            Intrinsic[0,0], Intrinsic[1,1] = cdata["focal_length_px"]
-            Intrinsic[0,2], Intrinsic[1,2] = cdata["principal_point_px"]
-
-            #Distortion parameters
-            Distortion = np.array(cdata["distortion_coeffs"])
-
-            #Rotation matrix and translation vector to convert from sensor 
-            #to common coordinate frame
-            Rotation = np.array(cdata["RT_body_from_sensor"])[:3,:3]
-            Translation = np.array(cdata["RT_body_from_sensor"])[:3,3]
-
-            CameraInfo[cname] = {
-                 "intrinsic": Intrinsic,
-                 "distortion": Distortion,
-                 "rotation": Rotation,
-                 "translation": Translation,
-                 }
-
-    return CameraInfo
+    return mask_files
 
 
-def undist(img_in, CameraMatrix, Dist):
-    """Undistort and crop image"""
-    h, w = img_in.shape[:2]
-    newCameraMatrix, roi = cv.getOptimalNewCameraMatrix(CameraMatrix, Dist, (w,h), 1, (w,h))
 
-    #Undistort
-    #img_out = cv.undistort(img_in, CameraMatrix, Dist, None, newCameraMatrix)
+class Calibration():
+    def __init__(self, calibration_file, camera_names):
+        """Get intrinsic matrix and distortion parameters from calibration file"""
+        self.CameraInfo = {}
+        self.Intrinsic = {}
+        self.Distortion = {}
+        self.Rotation = {}
+        self.Translation = {}
+        self.ObstructionMask = {}
 
-    #Undistort using remap
-    mapx, mapy = cv.initUndistortRectifyMap(CameraMatrix, Dist, None, newCameraMatrix, (w,h), 5)
-    img_out = cv.remap(img_in, mapx, mapy,
+        with open(calibration_file) as in_file:
+            data = json.load(in_file)
+            for cname in camera_names:
+                cdata = data[cname]
+
+                #Construct intrinsic matrix
+                self.Intrinsic[cname] = np.eye(3)
+                self.Intrinsic[cname][0,0], self.Intrinsic[cname][1,1] = cdata["focal_length_px"]
+                self.Intrinsic[cname][0,2], self.Intrinsic[cname][1,2] = cdata["principal_point_px"]
+
+                #Distortion parameters
+                self.Distortion[cname] = np.array(cdata["distortion_coeffs"])
+
+                #Rotation matrix and translation vector to convert from 
+                #sensor to common coordinate frame
+                self.Rotation[cname] = np.array(cdata["RT_body_from_sensor"])[:3,:3]
+                self.Translation[cname] = np.array(cdata["RT_body_from_sensor"])[:3,3]
+
+                #Obstruction mask
+                self.ObstructionMask[cname] = cdata["custom_vars"]["obstruction_mask_file"]
+
+
+    def undist(self, img_in, cam_name):
+        """Undistort and crop image, handle fisheye cameras separately"""
+        h, w = img_in.shape[:2]
+        CamMat = self.Intrinsic[cam_name]
+
+        if "FISHEYE" in cam_name:
+            Dist = self.Distortion[cam_name][:-1]
+            #newCamMat = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(CamMat, Dist, (w,h), np.eye(3))
+            #Undistort
+            mapx, mapy = cv.fisheye.initUndistortRectifyMap(CamMat, Dist, None, CamMat, (w,h), cv.CV_16SC2) #or cv.CV_32FC1
+            img_out = cv.remap(img_in, mapx, mapy,
                        interpolation=cv.INTER_LINEAR,
                        borderMode=cv.BORDER_CONSTANT)
 
-    # crop image
-    x, y, w, h = roi
-    img_out = img_out[y:y+h, x:x+w]
+        else:
+            Dist = self.Distortion[cam_name]
+            newCamMat, roi = cv.getOptimalNewCameraMatrix(CamMat, Dist, (w,h), 1, (w,h))
 
-    return img_out
+            #Undistort
+            #img_out = cv.undistort(img_in, CamMat, Dist, None, newCamMat)
 
-
-def undist_fe(img_in, CameraMatrix, Dist):
-    """Undistort and crop image for fisheye cameras"""
-    h, w = img_in.shape[:2]
-    #newCameraMatrix = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(CameraMatrix, Dist[:-1], (w,h), np.eye(3))
-
-    #Undistort
-    mapx, mapy = cv.fisheye.initUndistortRectifyMap(CameraMatrix, Dist[:-1], None, CameraMatrix, (w,h), cv.CV_16SC2) #or cv.CV32FC1
-    img_out = cv.remap(img_in, mapx, mapy,
+            #Undistort using remap
+            mapx, mapy = cv.initUndistortRectifyMap(CamMat, Dist, None, newCamMat, (w,h), 5)
+            img_out = cv.remap(img_in, mapx, mapy,
                        interpolation=cv.INTER_LINEAR,
                        borderMode=cv.BORDER_CONSTANT)
 
-    return img_out
+            # crop image
+            x, y, w, h = roi
+            img_out = img_out[y:y+h, x:x+w]
+
+        return img_out
 
 
 if __name__ == "__main__":
@@ -81,30 +97,34 @@ if __name__ == "__main__":
                     "F_MIDRANGECAM_C",
                     "M_FISHEYE_L",
                     "M_FISHEYE_R"]
-    camera_names_1 = ["B_MIDRANGECAM_C", "F_MIDLONGRANGECAM_CL",
-                      "F_MIDRANGECAM_C"]
-    camera_names_2 = ["M_FISHEYE_L", "M_FISHEYE_R"]
-    camera_names_3 = ["B_MIDRANGECAM_C", "F_MIDRANGECAM_C"]
     img_dir = dat_dir + "camera/"
     cal_dir = dat_dir + "calibration/"
 
     #get intrinsic matrix and distortion parameters
-    CameraInfo = get_calibration_data(cal_dir+"calibration.json",
-                                      camera_names)
+    CamCal = Calibration(cal_dir+"calibration.json", camera_names)
+
+    #Get mask and ref file names
+    MaskFiles = get_masks(cal_dir+"calibration.json", camera_names)
 
     #Load and undistort images
     for cname in camera_names:
+        #load image
         img_file = img_dir + cname + "/" + cname + "_0037133.jpg"
         img = cv.imread(img_file)
 
-        CameraMatrix = CameraInfo[cname]["intrinsic"]
-        Dist         = CameraInfo[cname]["distortion"]
-        if cname in camera_names_1:
-            dst = undist(img, CameraMatrix, Dist)
-            cv.imwrite(os.getcwd()+"/"+cname+"_1.png", dst)
-        elif cname in camera_names_2:
-            dst = undist_fe(img, CameraMatrix, Dist)
-            cv.imwrite(os.getcwd()+"/"+cname+"_2.png", dst)
+        #undistort image
+        dst = CamCal.undist(img, cname)
+        cv.imwrite(os.getcwd()+"/"+cname+"_1.png", dst)
 
+        #original image for comparison
         cv.imwrite(os.getcwd()+"/"+cname+"_0.png", img)
+
+        #apply misalign reference
+        #!!!SIZE MISMATCH!!!
+        #mdir = cal_dir + "vehicle_database/VEHICLES/Jarvis/"
+        #mis_ref = cv.imread(mdir + MaskFiles[cname]["mis_ref"])
+        #mis_ref = np.array(mis_ref).astype(float)/np.max(mis_ref)
+        #mr_dif = np.array(img)*mis_ref
+
+        #cv.imwrite(os.getcwd()+"/"+cname+"mr.png", mr_dif.tolist())
 
